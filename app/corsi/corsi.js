073788dc377e15859b2eda435c4421913efd6239
g.mell1 <shottyno90@gmail.com>
2019-11-24 17:27:38 +0100

    GUI updated

--- app/corsi/corsi.js
+++ app/corsi/corsi.js
@@ -41,11 +41,10 @@ function getCourses()
 {
     let courses = global.freqExams;
     const act_sem = appSettings.getString("semestre");
-    console.log(act_sem);
 
     for (let i=0; i<courses.length; i++)
     {
-        if (act_sem == "Secondo Semestre" && (courses[i].semestre == "S2" || courses[i].semestre == "A2" || courses[i].semestre == "N/A"))
+        if (act_sem === "Secondo Semestre" && (courses[i].semestre === "S2" ||courses[i].semestre === "A2" || courses[i].semestre === "N/A"))
         {
             items.push({
                 "anno": drawYear(courses[i].annoId),
@@ -58,8 +57,20 @@ function getCourses()
             });
             esamiList.refresh();
         }
-        else if (act_sem == "Primo Semestre" && (courses[i].semestre == "S1" || courses[i].semestre == "A1" || courses[i].semestre == "N/A"))
+        else if (act_sem === "Primo Semestre" && (courses[i].semestre === "S1" || courses[i].semestre === "A1" || courses[i].semestre === "N/A"))
         {
+            items.push({
+                "anno": drawYear(courses[i].annoId),
+                "esame": courses[i].nome,
+                "prof": courses[i].docente,
+                "data_inizio": "Dal " + courses[i].inizio,
+                "data_fine": " al " + courses[i].fine,
+                "ult_mod": courses[i].modifica,
+                "adLogId": courses[i].adLogId
+            });
+            esamiList.refresh();
+        }
+        else if (act_sem === undefined){
             items.push({
                 "anno": drawYear(courses[i].annoId),
                 "esame": courses[i].nome,
@@ -73,18 +84,18 @@ function getCourses()
         }
         else
         {
-            console.log(courses[i].nome + " :Esame non presente nel semestre attuale!!");
+            console.log(courses[i].nome + " :Esame non aggiunto al semestre attuale!!");
         }
     }
 
 }
 function drawYear(year)
 {
-    if (year == 1)
+    if (year === 1)
         return "I";
-    else if (year == 2)
+    else if (year === 2)
         return "II";
-    else if (year == 3)
+    else if (year === 3)
         return "III";
     else
         return "SCE";
@@ -106,8 +117,21 @@ function onItemTap(args) {
 exports.onItemTap = onItemTap;
 
 function drawTitle() {
-    page.getViewById("aa").text = "A.A. " + appSettings.getString("aa_accad");
-    page.getViewById("semestre").text = appSettings.getString("semestre");
+    if (appSettings.getString("aa_accad") !== undefined)
+        page.getViewById("aa").text = "A.A. " + appSettings.getString("aa_accad");
+    else
+    {
+        console.log("CORSI.AA_ACCAD = undefined (A.A non recuperato!)");
+        page.getViewById("aa").text = "A.A. Non Disponibile";
+    }
+
+    if (appSettings.getString("semestre") !== undefined)
+        page.getViewById("semestre").text = appSettings.getString("semestre");
+    else
+    {
+        console.log("CORSI.SEMESTRE = undefined (Semestre non recuperato!)");
+        page.getViewById("semestre").text = "Semestre Non Disponibile";
+    }
 }
 exports.tapBus = function(){
     const nav =
